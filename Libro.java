1d1caa4535e5a17170c3c6e0508268e78b1abf89
Camilo-Piedrahita <156150509+Camilo-Piedrahita@users.noreply.github.com>
2024-01-11 21:42:49 -0500

    Add files via upload

---
+++ Libro.java
@@ -0,0 +1,17 @@
+public class Libro {
+    private String autor;
+    private String titulo;
+    private int isbn;
+    private boolean prestado;
+
+
+    public Libro(String autor, String titulo, int isbn, boolean prestado) {
+        this.autor = autor;
+        this.titulo = titulo;
+        this.isbn = isbn;
+        this.prestado = prestado;
+    }
+
+}
+
+
